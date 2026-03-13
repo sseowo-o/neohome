55ec4e2fe4848e6b395d23e783211064a1500e1d
seo-woo <138635567+sseowo-o@users.noreply.github.com>
2026-03-13 14:13:57 +0900

    -

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/neohome',
+  assetPrefix: '/neohome/',
   images: {
     unoptimized: true,
   },
