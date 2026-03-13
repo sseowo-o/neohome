9d4353f9ac44ad77491dd3ac6bf909cb0253ce9d
seo-woo <138635567+sseowo-o@users.noreply.github.com>
2026-03-13 15:53:39 +0900

    chore: setup .gitignore and remove unnecessary files

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/neohome',
-  assetPrefix: '/neohome/',
   trailingSlash: true,
   images: {
     unoptimized: true,
